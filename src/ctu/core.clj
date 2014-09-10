5f0e22620de81e3c763472a89837feb654e2c01a
Christophe Turle <cturle@yahoo.com>
2014-09-10 15:07:34 +0200

    timeout-fn + *context* as atom

--- src/ctu/core.clj
+++ src/ctu/core.clj
@@ -1,16 +1,20 @@
 (ns ctu.core)
 
 
-(def ^:dynamic *context* {})
+(def +out+ *out*)
+(defn dprintln [& more] (binding [*out* +out+] (apply println more)))
+(defn dprint   [& more] (binding [*out* +out+] (apply print more)))
+
+(def ^:dynamic *context* (atom {}))
 
 (defn cget
 "Any CM -> (or nil Map).
-  Returns the map attached to CM in the context, or nil if not found.
+  Returns the map attached to CM in the Context *context*, or nil if not found.
  [Any CM, Any K] -> (or nil Any).
   Returns the value mapped to key K in the contextual map CM. nil is returned as default-value.
  Throw an exception if CM is not found."
  ([CM]
-  (if-let [M (get *context* CM)] M (throw (Exception. (str "cget: concept not found : " CM)))) )
+  (if-let [M (get (deref *context*) CM)] M (throw (Exception. (str "cget: concept not found : " CM)))) )
  ([CM K]
   (get (cget CM) K) ))
 
@@ -38,6 +42,8 @@
   [F F-PRE?]
   (fn [INIT L]
     (loop [X INIT, L L]
+      (when (.. Thread currentThread isInterrupted)
+        (throw (ex-info "interruption in reduce-pre" {:type :interruption, :X X :L L})) )
       (if (empty? L) {:all-pre-ok? true, :out X}
         (let [Y (first L)]
           (if (not (F-PRE? X Y)) {:all-pre-ok? false, :bad-pre [X, Y]}
@@ -45,11 +51,15 @@
 
 
 (defn choice-v
-  "[State S0, (State -> Choice) S-C, ([State Choice] -> State) SC-S, (State -> Boolean) FS?] -> vector-of Choice C-v
-  Returns a sequence of choice from an initial State S0, a Choice chooser, a new state builder SC-S and
-  a final state predicate FS?."
+  "[State S0, (State -> Choice) S-C, ([State, Choice] -> State) SC-S, (State -> Boolean) FS?]
+    -> vector-of Choice C-v
+  Returns a sequence of choice from an initial State S0, a Choice chooser S-C, a new state builder SC-S and
+  a final state predicate FS?.
+  Interruptible."
   [S0, S-C, SC-S, FS?]
   (loop [S S0, C-v []]
+    (when (.. Thread currentThread isInterrupted)
+      (throw (ex-info "interruption in choice-v" {:type :interruption, :S S :C-v C-v})) )
     (if (FS? S)
       C-v
       (let [C  (S-C S)
@@ -57,6 +67,19 @@
         (recur NS (conj C-v C)) ))))
 
 
+(defn timeout-fn
+  "[Function F, int TO] -> Function G.
+   Returns a function G which is the same as F if it returns in less than TO milli-seconds else it returns nil.
+   special variables are rebound when G is executed not defined."
+  [F TO]
+  (fn [& MORE]
+    (let [FB (bound-fn* F)
+          FU (future (apply FB MORE))
+          R  (deref FU TO :timed-out) ]
+      (if (= :timed-out R)
+        (do (future-cancel FU) nil)
+        R )
+      )))
 
 
 
--- test/ctu/core_test.clj
+++ test/ctu/core_test.clj
@@ -5,52 +5,75 @@
 
 (def +CTXT1+ {:c1 {:val* #{1 2}, :val 3}})
 
+; (cget-tests)
 (deftest cget-tests
-  (binding [*context* +CTXT1+]
+  (binding [*context* (atom +CTXT1+)]
     (testing "CASE : cget with one argument"
       (is (= (cget :c1) (get +CTXT1+ :c1)))
-      (is (try (cget :c2) false (catch Exception e true))) )
+      (is (thrown? Exception (cget :c2))) )
     (testing "CASE : cget with two arguments"
       (is (= (cget :c1 :val) 3))
       (is (nil? (cget :c1 :val2)))
-      (is (try (cget :c2 :val) false (catch Exception e true))) )))
+      (is (thrown? Exception (cget :c2 :val))) )))
+
 
 (deftest cget*-tests
-  (binding [*context* +CTXT1+]
+  (binding [*context* (atom +CTXT1+)]
     (testing "cget*"
       (is (= (cget* :c1 :val*) #{1 2}))
       (is (let [R (cget* :c1 :val*-unknown)] (and (set? R) (= #{} R))))
-      (is (try (cget* :c2 :val*) false (catch Exception e true)))
+      (is (thrown? Exception (cget* :c2 :val*)))
       )))
 
 (deftest cget-v-tests
-  (binding [*context* +CTXT1+]
+  (binding [*context* (atom +CTXT1+)]
     (testing "cget-v"
       (is (= (cget-v :c1 :val*) #{1 2}))
       (is (let [R (cget-v :c1 :val*-unknown)] (and (vector? R) (= [] R))))
-      (is (try (cget-v :c2 :val*) false (catch Exception e true)))
+      (is (thrown? Exception (cget-v :c2 :val*)))
       )))
 
+
+(def ^:dynamic *V* 0)
+
+; (timeout-fn-tests)
+(deftest timeout-fn-tests
+  (let [F (timeout-fn (fn [T] (. Thread (sleep T)) T) 1000)]
+    (testing "CASE : return value"
+      (is (= 500 (F 500))) )
+    (testing "CASE : interruption"
+      (is (nil? (F 3000))) ))
+  (testing "CASE : bindings are preserved"
+    (let [F (timeout-fn (fn [] *V*) 1000)]
+      (binding [*V* 2]
+        (is (= 2 (F))) )))
+  )
+
+
+(declare div-pre?)
+
+; (reduce-pre-tests)
+(deftest reduce-pre-tests
+  (testing "(reduce-pre / div-pre?)"
+    (let [F (timeout-fn (reduce-pre / div-pre?) 1000)]
+      (is (= 1 (:out (F 8 [1 2 4]))))
+      (is (:all-pre-ok? (F 8 [1 2 4])))
+      (is (not (:all-pre-ok? (F 8 [1 0 4]))))
+      (is (= [8 0] (:bad-pre (F 8 [1 0 4]))))
+      (is (not (:all-pre-ok? (F 8 [1 :x 4]))))
+      (is (= [8 :x] (:bad-pre (F 8 [1 :x 4]))))
+    )))
 
 (defn div-pre? [X Y] (and (number? X)
                           (number? Y)
                           (not= Y 0) ))
 
-(deftest reduce-pre-tests
-  (testing "reduce-pre"
-    (is (= 1 (:out ((reduce-pre / div-pre?) 8 [1 2 4]))))
-    (is (:all-pre-ok? ((reduce-pre / div-pre?) 8 [1 2 4])))
-    (is (not (:all-pre-ok? ((reduce-pre / div-pre?) 8 [1 0 4]))))
-    (is (= [8 0] (:bad-pre ((reduce-pre / div-pre?) 8 [1 0 4]))))
-    (is (not (:all-pre-ok? ((reduce-pre / div-pre?) 8 [1 :x 4]))))
-    (is (= [8 :x] (:bad-pre ((reduce-pre / div-pre?) 8 [1 :x 4]))))
-    ))
 
 ; (choice-v-tests)
 (deftest choice-v-tests
-  (testing "CASE : nominal"
-    (is (= [inc inc] (choice-v 1 (fn [S] inc) (fn [S C] (C S)) (fn [S] (= 3 S))))) ))
-
+  (testing "CASE : nominal, F = choice-v"
+    (let [F (timeout-fn choice-v 1000)]
+      (is (= [inc inc] (F 1 (fn [S] inc) (fn [S C] (C S)) (fn [S] (= 3 S))))) )))
 
 
 (comment
